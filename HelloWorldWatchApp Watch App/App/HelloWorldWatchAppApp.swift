686952799b371f83747ad7d3070030975dfbaa22
Jacek Kaczmarek <kaczmarekjack@icloud.com>
2025-03-11 14:54:03 +0000

    add App folder

---
+++ HelloWorldWatchApp Watch App/App/HelloWorldWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldWatchAppApp.swift
+//  HelloWorldWatchApp Watch App
+//
+//  Created by Jacek Kaczmarek on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreenView()
+        }
+    }
+}
